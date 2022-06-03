8ea1bba94f5276d4f44b56d715342a76479ee1ca
JIHYO-JEON <jihyo@gm.gist.ac.kr>
2022-06-03 15:04:44 +0100

    :heavy_plus_sign: Add dotenv
    Add dotenv to user env file

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
+import 'dotenv/config';
 import app from './app';
 import { createServer } from 'http';
 
 const PORT: number = Number(process.env.PORT) || 3000;
 
 const server = createServer(app);
+const url =
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:3000'
+    : 'https://thunder-chat-server.onrender.com/';
 
 server.listen(PORT, () => {
-  console.log(`🚀 server running on http://localhost:${PORT}`);
+  console.log(`🚀 server running on ${url}`);
 });
 
 export default server;
