1a290347216475c9c847eed5a23525fea24a3358
JIHYO-JEON <jihyo@gm.gist.ac.kr>
2022-06-05 10:34:21 +0100

    :fire: Delete codes not using for now

--- src/controllers/chat.ts
+++ src/controllers/chat.ts
@@ -18,21 +18,6 @@ export async function getAllChatInTheRoom(
   }
 }
 
-export async function getChat(req: express.Request, res: express.Response) {
-  try {
-    // TODO: Get a Chat
-    res.status(200);
-    // TODO: send a Chat
-    // res.send(...);
-    res.send('Get a Chat');
-    res.end();
-  } catch (error) {
-    res.status(500);
-    res.send(error);
-    res.end();
-  }
-}
-
 export async function addChat(req: express.Request, res: express.Response) {
   try {
     // TODO: create a Chat
@@ -40,19 +25,6 @@ export async function addChat(req: express.Request, res: express.Response) {
     // TODO: send a Chat
     // res.send(...);
     res.send('Create a Chat');
-    res.end();
-  } catch (error) {
-    res.status(500);
-    res.send(error);
-    res.end();
-  }
-}
-
-export async function deleteChat(req: express.Request, res: express.Response) {
-  try {
-    // TODO: Delete a Chat
-    res.status(204);
-    res.send('Delete the Chat');
     res.end();
   } catch (error) {
     res.status(500);
