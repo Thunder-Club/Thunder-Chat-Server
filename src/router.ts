e5f92191017e96da262793f848db5b3675f2f048
JIHYO-JEON <jihyo@gm.gist.ac.kr>
2022-06-05 10:33:15 +0100

    :art: Update routing

--- src/router.ts
+++ src/router.ts
@@ -10,21 +10,16 @@ router.get(
   },
 );
 
-router.post('/room', controllers.createRoom);
 router.get('/room/:id', controllers.getRoom);
-router.patch('/room/:id', controllers.updateRoom);
-router.delete('/room/:id', controllers.deleteRoom);
 
 router.get('/room/:id/user', controllers.getAllUserInTheRoom);
 router.get('/user/:id', controllers.getUser);
-router.post('/room/:id/user', controllers.addUser);
+router.post('/user', controllers.addUser);
 router.patch('/user/:id', controllers.updateUser);
 router.delete('/user/:id', controllers.deleteUser);
 
 router.get('/room/:id/chat', controllers.getAllChatInTheRoom);
-router.get('/chat/:id', controllers.getChat);
 router.post('/room/:id/chat', controllers.addChat);
-router.delete('/chat/:id', controllers.deleteChat);
 
 router.get(
   '**',
