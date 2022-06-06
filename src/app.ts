9d00608e376c754530e794e2e18038fd41655c1c
Jihyo Jeon <jihyo@gm.gist.ac.kr>
2022-06-06 15:21:06 +0100

    :sparkles: Automate API documentation

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,13 @@
 import * as express from 'express';
 import router from './router';
+import * as swaggerUi from 'swagger-ui-express';
+import * as YAML from 'yamljs';
+
+const swaggerSpec = YAML.load('../../Thunder-Chat-API.yaml');
 
 const app: express.Application = express();
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use(express.json());
 app.use('/', router);
 
