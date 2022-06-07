713ccfc57e262bd5eb596f48555a2f0721cadde6
Jihyo Jeon <jihyo@gm.gist.ac.kr>
2022-06-07 17:28:48 +0100

    :sparkles: route for api docs

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,15 @@
+import 'dotenv/config';
 import * as express from 'express';
 import router from './router';
 import * as swaggerUi from 'swagger-ui-express';
 import * as YAML from 'yamljs';
 
-const swaggerSpec = YAML.load('../../Thunder-Chat-API.yaml');
+const swaggerSpec = YAML.load(process.env.API_DOC_YAML);
 
 const app: express.Application = express();
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use(express.json());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use('/', router);
 
 export default app;
